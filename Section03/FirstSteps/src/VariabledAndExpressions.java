2012dc3758c24c12f6a7a368e22532b4be3d5d8a
Vitor Hugo Jovanelli <65049630+VitorJovanelli@users.noreply.github.com>
2025-10-23 07:01:27 +0100

    Created a class to practise using variabled and expressions.

---
+++ Section03/FirstSteps/src/VariabledAndExpressions.java
@@ -0,0 +1,20 @@
+public class VariabledAndExpressions {
+
+    public static void main(String[] args) {
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        myThirdNumber = myFirstNumber * 2;
+
+        myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+
+        int myLastOne = 1000 - myTotal;
+        System.out.println(myLastOne);
+    }
+
+}
