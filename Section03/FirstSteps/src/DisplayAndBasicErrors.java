1d7586e8fd14c2679f988ec85ade2dbe28507913
Vitor Hugo Jovanelli <65049630+VitorJovanelli@users.noreply.github.com>
2025-10-23 06:44:48 +0100

    Created my first class and tried to display the Hello world phrase.

---
+++ Section03/FirstSteps/src/DisplayAndBasicErrors.java
@@ -0,0 +1,9 @@
+public class DisplayAndBasicErrors {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello, Vitor!");    // """ is used to define a string literal
+        System.out.println('c');                // "'" can only be used for characters literal
+    }
+
+}
